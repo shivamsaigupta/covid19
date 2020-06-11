4008d78365ba493d67d5443ce6d57c4903967dd0
Shivam Sai Gupta <contact@shivam.me>
2020-06-11 15:17:54 +0530

    Updates

---
+++ precache-manifest.317993171f172663c4ee7b2e37970d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bd13e2c2dd4b50a2a525e0533f1ae8",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "26d03b64b7e1c375b4d4",
+    "url": "/covid19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f468dfac9fc82fc6870b",
+    "url": "/covid19/static/css/main.c3de8099.chunk.css"
+  },
+  {
+    "revision": "26d03b64b7e1c375b4d4",
+    "url": "/covid19/static/js/2.aabba59f.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid19/static/js/2.aabba59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f468dfac9fc82fc6870b",
+    "url": "/covid19/static/js/main.9e0ce5d7.chunk.js"
+  },
+  {
+    "revision": "3f9ea9de9b45ec0fb219",
+    "url": "/covid19/static/js/runtime-main.59095949.js"
+  }
+]);
